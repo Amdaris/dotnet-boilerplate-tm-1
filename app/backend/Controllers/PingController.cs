b20be35828f339c2ec87a373daa58a4f82fcbd05
Alexandru Nicoara <28361885+alexncr2@users.noreply.github.com>
2024-02-02 17:42:30 +0200

    Initial commit

---
+++ app/backend/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    public class PingController : Controller
+    {
+        [HttpGet("ping")]
+        public string Get()
+        {
+            // Don't change this
+            return "pong";
+        }
+    }
+}
